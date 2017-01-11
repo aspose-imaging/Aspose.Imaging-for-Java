b5bf0e787ff1cbdadadb0c8a52871669c9efab09
Ikram ul Haq <mikram_ul_haq@hotmail.com>
2017-01-11 10:52:21 +0500

    Added new release examples (16.12.0)

---
+++ Examples/src/main/java/com/aspose/imaging/examples/files/MeteredLicensing.java
@@ -0,0 +1,27 @@
+package com.aspose.imaging.examples.files;
+
+public class MeteredLicensing 
+{
+	public static void main(String[] args) throws Exception 
+        {
+		// Create an instance of OCR Metered class
+        com.aspose.imaging.Metered metered = new com.aspose.imaging.Metered();
+        
+        // Access the setMeteredKey property and pass public and private keys as parameters
+        metered.setMeteredKey("<valid pablic key>", "<valid private key>");
+
+        // Get consumed qantity value before accessing API
+        System.out.println("Consumption quantity" + com.aspose.imaging.Metered.getConsumptionQuantity());
+        
+        // DO PROCESSING
+        //com.aspose.imaging.Image img = com.aspose.imaging.Image.load("C:\\in.psd");
+        //img.save("C:\\Temp\\out.psd");
+        
+        //Since upload data is running on another thread, so we need wait some time
+        //java.lang.Thread.sleep(10000);
+        
+        // get metered data amount
+        System.out.println("Consumption quantity" + com.aspose.imaging.Metered.getConsumptionQuantity());
+	}
+
+}
---
+++ Examples/src/main/java/com/aspose/imaging/examples/metafile/ConvertEMFtoWMF.java
@@ -0,0 +1,35 @@
+package com.aspose.imaging.examples.metafile;
+
+import com.aspose.imaging.examples.Utils;
+import com.aspose.imaging.imageoptions.PdfOptions;
+
+public class ConvertEMFtoWMF 
+{
+	public static void main(String... args) throws Exception 
+        {
+		// List of existing EMF images. 
+        String path = "";
+        String[] files = new String[] { "TestEmfRotatedText.emf", "TestEmfPlusFigures.emf", "TestEmfBezier.emf" };
+
+        // Loop for each file name.
+        for (String file : files)
+        {
+            // Input file name & path.
+            String filePath = path + "\\" + file;
+            
+            // Load the EMF image as image and convert it to MetaImage object.
+            com.aspose.imaging.fileformats.emf.MetaImage image = 
+                    (com.aspose.imaging.fileformats.emf.MetaImage)com.aspose.imaging.Image.load(filePath, new com.aspose.imaging.imageloadoptions.MetafileLoadOptions(true));
+	try
+	{
+            // Convert the EMF image to WMF image by creating and passing WMF image options class object.
+            image.save(filePath + "_out.wmf", new com.aspose.imaging.imageoptions.WmfOptions());
+	}
+	finally
+	{
+            // clean the resources
+            image.dispose();
+	}
+	}
+
+}
