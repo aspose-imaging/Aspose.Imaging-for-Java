52f68ed5f04ce9a165e78e60df8bfc33d5ccde09
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2018-02-20 16:04:08 +0500

    Added new feature as per Aspose.Imaging for Java 18.2 release
    ConvertImageWithGrayscale.java
    SupportTiffDeflate .java
    PSDtoPDF.java

---
+++ Examples/src/main/java/com/aspose/imaging/examples/ModifyingImages/ConvertImageWithGrayscale.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aspose.imaging.examples.ModifyingImages;
+import com.aspose.imaging.Image;
+import com.aspose.imaging.examples.Utils;
+import com.aspose.imaging.fileformats.jpeg.JpegCompressionColorMode;
+import com.aspose.imaging.imageoptions.JpegOptions;
+
+public class ConvertImageWithGrayscale {
+    
+public static void main(String... args) throws Exception {
+
+    //ExStart:ConvertImageWithGrayscale
+ String dataDir = Utils.getSharedDataDir(ConvertImageWithGrayscale.class) + "ModifyingImages/"; 
+ String srcDir = "IMAGINGNET-2160\\";
+ String outputFolder = "C:\\temp\\";
+ 
+      int[] colorTypes = new int[]
+ {
+  JpegCompressionColorMode.Grayscale,
+  JpegCompressionColorMode.YCbCr,
+  JpegCompressionColorMode.Rgb,
+  JpegCompressionColorMode.Cmyk,
+  JpegCompressionColorMode.Ycck,
+    };
+ 
+String[] sourceFileNames = new String[]
+{
+"Rgb.jpg",
+"Rgb.jpg",
+"Rgb.jpg",
+"Rgb.jpg",
+"Rgb.jpg",
+};
+ 
+JpegOptions options = new JpegOptions();
+options.setBitsPerChannel((byte)12);
+ 
+for (int i = 0; i < colorTypes.length; ++i)
+{
+options.setColorType(colorTypes[i]);
+ 
+String fileName = JpegCompressionColorMode.getName(JpegCompressionColorMode.class, colorTypes[i]) + " 12-bit.jpg";
+String referencePath = srcDir + fileName;
+String outputPath = outputFolder + fileName;
+ 
+Image image = Image.load(srcDir + sourceFileNames[i]);
+try
+{
+image.save(outputPath, options);
+}
+finally
+{
+image.dispose();
+}
+}
+
+    //ExEnd:ConvertImageWithGrayscale
+}
+}
