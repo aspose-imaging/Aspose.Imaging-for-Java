37339d4321f3a9f6518bda5cc4ecba0865988f7c
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-07-04 22:04:03 +0500

    Added utility file w.r.t 19.6

---
+++ Examples/src/main/java/com/aspose/imaging/examples/Assert.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aspose.imaging.examples;
+
+/**
+ *
+ * @author mfazi
+ */
+public class Assert {
+    public static boolean areEqual(Object left, Object right) {
+        return left == right;
+    }
+
+    public static boolean areEqual(Object left, Object right, Object epsilon) {
+        return left == right;
+    }
+
+    public static boolean areNotEqual(Object left, Object right) {
+        return left == right;
+    }
+}
